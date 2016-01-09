eacec73033ec5d572c95fa055193177233ec1f98
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2016-01-09 23:15:56 +0100

    MapManager.visible(): also include maps where current user is owner or editor

--- leaflet_storage/managers.py
+++ leaflet_storage/managers.py
@@ -4,5 +4,7 @@ from django.db.models import Q
 class MapManager(models.GeoManager):
     def visible(self, request):
         filter = Q(share_status=self.model.PUBLIC)
+        filter = filter | Q(owner=request.user)
+        filter = filter | Q(editors=request.user)
 
         return self.filter(filter)
